ab4fbef55301afe23bb9423007e04d92fa4b05dc
Chaveejay <chawwa53@gmail.com>
2025-02-08 19:11:10 +0530

    async await for review controller ✔️

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -47,45 +47,49 @@ export async function getReviews(req,res){
 }
 }
 
-export function deleteReviews(req,res){
+export async function deleteReviews(req,res){
     const email = req.params.email;
 
-    if(req.user==null){
+    try{
+    if (req.user ==null){
         res.status(401).json({
-            message : "Please login and try again"
-        });
+                message : "Please login and try again"
+           });
     }
-
-    if (req.user.role == "admin"){
-        Review.deleteOne({email : email}).then(()=>{
-            res.json({
-                message : "Review Deleted Successfully"
-            })
-        }).catch(()=>{
-            res.status(500).json({
-                error : "Review deletion failed"
-            });
-        });
-        return;
-    }
-
-    if(req.user.role =="customer"){
-        if(req.user.email ==email){
-            Review.deleteOne({email : email}).then(()=>{
-                res.json({
-                    message : "Review Deleted Successfully"
-                })
-            }).catch(()=>{
-                res.status(500).json({
-                    error : "Review deletion failed"
-                });
-            });
-        }
-    }else{
-        res.status(403).json({
-            message : "You are not authorized to perform this action"
+    else if (req.user.role == "admin"){
+        const result = await Review.deleteOne({
+            email : email
         })
+        if(result.deletedCount>0){ res.json({
+            message : "Review deleted Successfully"
+        })}
+        else{
+            res.json({
+                message : "Review not found or already deleted"
+            })
+        }
     }
+    else if (req.user.role == "customer"){
+        if(req.user.email == email){
+            const result = await Review.deleteOne({
+                email : email
+            })
+            if(result.deletedCount>0){ res.json({
+                message : "Review deleted Successfully"
+            })}
+            else{
+                res.json({
+                    message : "Review not found or already deleted"
+                })
+            }
+            
+        }else{
+            res.json({ error: "You don't have access to perform this action"})
+        }
+    }
+}catch(err){
+    res.json(err.message)
+}
 }
 
 export async function approveReview(req,res){
--- routes/reviewRouter.js
+++ routes/reviewRouter.js
@@ -6,6 +6,6 @@ const reviewRouter = express.Router();
 reviewRouter.post("/",addReview);
 reviewRouter.get("/",getReviews);
 reviewRouter.put("/approve/:email",approveReview);
-reviewRouter.delete("/:email",deleteReviews);
+reviewRouter.delete("/delete/:email",deleteReviews);
 
 export default reviewRouter;
